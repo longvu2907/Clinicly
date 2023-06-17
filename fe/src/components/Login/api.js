441c62e055e42bea91db652844d5f02e27d3fe7f
Huu Le <huu.le@devsamurai.com>
2023-06-17 17:41:51 +0700

    create api.js

---
+++ fe/src/components/Login/api.js
@@ -0,0 +1,17 @@
+export const loginAPI = async (email, password) => {
+  const response = await fetch('/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  if (!response.ok) {
+    const error = await response.json();
+    throw new Error(error.message);
+  }
+
+  const data = await response.json();
+  return data.data;
+};
